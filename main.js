506bf10255cd054bc6f2370e4476895ba278e2ea
Brennen Murphy <brennenamurphy@gmail.com>
2017-10-23 22:04:43 -0600

    added comments

--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 $(document).ready(function() {
 
-
-
+// hide the input on page load
 $('.input').hide();
 
-
-
+// hide the static text and reveal text box when cursor hovers over
 $('.noInput').hover(function() {
   $('.noInput').hide();
   $('.input').show();
 });
 
-
-
-
+// captures keypresses and updates the noInput span with the new text
 $('.input').keyup(function() {
   var currentText = $( this ).val();
   $('.noInput').text( currentText );
   console.log(currentText)
 });
 
+
+// when input loses focus the input is hidden and the noInput span is revealed.
 $('.input').focusout(function() {
   $('.input').hide();
   $('.noInput').show();
